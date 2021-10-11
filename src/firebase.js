0c52797289fb941e1897bc007b33f3ba3f01293b
icaroglauco-lab <icaroglauco@gmail.com>
2021-10-11 20:48:54 -0300

    Setup de firebase, pagejs e mockdata

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmkPVPvbu9gFTU4n5KJ5ftJcm710qpvjQ",
+  authDomain: "joaldomendonca-b7da4.firebaseapp.com",
+  projectId: "joaldomendonca-b7da4",
+  storageBucket: "joaldomendonca-b7da4.appspot.com",
+  messagingSenderId: "596772575496",
+  appId: "1:596772575496:web:6c9ff72ce35e388c5c74bd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/main.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Header from "./componentes/header.svelte";
+    import Footer from "./componentes/footer.svelte";
+
+    import LandingHeader from './componentes/HomeHeader.svelte';
+    import { setContext } from "svelte";
+    import Data from './mockdata.json';
+
+    import router from "page"
+
+    let mockdata = Data;
+
+    let state = setContext('state', mockdata);
+
+    let page;
+
+    router('/', () => page = LandingHeader)
+    router.start()
+
+</script>
+
+<svelte:component this={page} />
+
+<Footer></Footer>
